8acce18f1863cbf63f17957010a2b3a0c36fa622
tony <ganchengkai@joinhov.com>
2019-10-29 11:39:45 +0800

    Initial Commit

---
+++ HelloLib/HelloLib.h
@@ -0,0 +1,19 @@
+//
+//  HelloLib.h
+//  HelloLib
+//
+//  Created by tony on 2019/10/29.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloLib.
+FOUNDATION_EXPORT double HelloLibVersionNumber;
+
+//! Project version string for HelloLib.
+FOUNDATION_EXPORT const unsigned char HelloLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloLib/PublicHeader.h>
+
+
