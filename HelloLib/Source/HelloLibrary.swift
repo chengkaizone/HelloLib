267bef2c1d8a405450dbf534dbd9ad33f8e138a9
tony <ganchengkai@joinhov.com>
2019-10-29 12:06:29 +0800

    add code

---
+++ HelloLib/Source/HelloLibrary.swift
@@ -0,0 +1,22 @@
+//
+//  HelloLibrary.swift
+//  HelloLib
+//
+//  Created by tony on 2019/10/29.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+import Foundation
+
+public func calAdd(a: Int, b: Int) ->Int {
+    return a + b
+}
+
+
+open class HelloLibrary {
+    
+    open func calSub(a: Int, b: Int) ->Int {
+        return a - b
+    }
+    
+}
