907a252f672e7bc881bf22a3aa94aa11f2504ede
tony <ganchengkai@joinhov.com>
2019-10-30 12:00:23 +0800

    add code

--- Source/HelloLibrary.swift
+++ Source/HelloLibrary.swift
@@ -13,13 +13,13 @@ public func calAdd(a: Int, b: Int) ->Int {
 }
 
 
-open class HelloLibrary {
+public class HelloLibrary {
     
     public init() {
         
     }
     
-    open func calSub(a: Int, b: Int) ->Int {
+    public func calSub(a: Int, b: Int) ->Int {
         return a - b
     }
     
