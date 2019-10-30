d3c0ed7f6aa190243f3f53db28b9393516e92eae
tony <ganchengkai@joinhov.com>
2019-10-30 11:32:56 +0800

    add code

--- Package.swift
+++ Package.swift
@@ -7,20 +7,18 @@ let package = Package(
     name: "HelloLib",
     platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HelloLib",
             targets: ["HelloLib"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "HelloLib", dependencies: [], path: "Source"),
+            name: "HelloLib", dependencies: ["Alamofire"], path: "Source"),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v4, .v4_2, .v5]
 )
